a74fe1bdd86ab67cce243c68b39af52374bbc26f
Sandya Choppara <sandyachoppara@gmail.com>
2023-12-28 13:18:57 +0800

    Initial commit

---
+++ src/main/java/com/app/entity/UserRegistration.java
@@ -0,0 +1,69 @@
+package com.app.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "user_reg")
+public class UserRegistration {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Integer id;
+
+	@NotEmpty
+	@Size(min = 5)
+	String name;
+
+	@NotNull
+	Long mobile;
+
+	@NotEmpty
+	@Email
+
+	String email;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(Long mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "UserRegistration [id=" + id + ", name=" + name + ", mobile=" + mobile + ", email=" + email + "]";
+	}
+
+}
